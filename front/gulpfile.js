02303e4b99f056b7eb77ca6c3eabd15c6497e0a2
AhmedAdel19 <adel.eng20@gmail.com>
2022-07-03 13:38:12 +0200

    firstPush

---
+++ front/gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+
+const gulpSass = require('gulp-sass');
+
+const sass = gulpSass(require('sass'));
+
+// gulp.task('comileSass',async function(){
+//     gulp.watch('src/*.scss', async function(){
+//         gulp.src('src/*.scss').pipe(sass()).pipe(gulp.dest('src/css'));
+//     })
+// })
+
+gulp.task('comileSass', async function () {
+    gulp.watch('src/components/**/*.scss', async function () {
+        gulp.src('src/components/**/*.scss').pipe(sass()).pipe(gulp.dest('src/css'));
+    });
+
+
+})
---
+++ front/src/App.js
@@ -0,0 +1,19 @@
+//import "./css/index.css"
+
+function App() {
+  return (
+    <div className="container">
+      <header>
+        Shooping Cart
+      </header>
+      <main>
+        content
+      </main>
+      <footer>
+        copyRight Ahmed Adel@2022
+      </footer>
+    </div>
+  );
+}
+
+export default App;
