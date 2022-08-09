42ed71cb0f2a67a0f3e73932a66c0fc61371b1c8
AhmedAdel19 <adel.eng20@gmail.com>
2022-08-09 13:08:26 +0200

    cartRedux

--- front/src/components/Products/Products.js
+++ front/src/components/Products/Products.js
@@ -4,6 +4,7 @@ import ProductModal from './ProductModal';
 import Bounce from 'react-reveal/Bounce';
 import { connect } from 'react-redux';
 import {fetchProducts} from '../../store/actions/products';
+import { addToCart } from './../../store/actions/cart';
 
 
 function Products(props) {
@@ -54,6 +55,7 @@ function Products(props) {
 }
 export default connect((state)=>{
     return{
-        products : state.products.filterProducts
+        products : state.products.filterProducts,
+        cartItems : state.cart.cartItems
     }
-},{fetchProducts})(Products);
+},{fetchProducts , addToCart})(Products);
