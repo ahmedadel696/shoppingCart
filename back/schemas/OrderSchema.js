f8bd523a6a2833fe1bc31044c9722942a41ad35a
AhmedAdel19 <adel.eng20@gmail.com>
2022-08-09 14:55:57 +0200

    orderModel

---
+++ back/schemas/OrderSchema.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = new mongoose.Schema({
+    name : String,
+    email : String,
+},{
+    timestamps:true
+});
+
+module.exports = OrderSchema;
--- back/server.js
+++ back/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
 const routers = require('./routes/routes');
-
+const db_config = require('./config/db')
 
 app.use(bodyParser.json());
 
@@ -12,11 +12,13 @@ app.use('/', routers);
 app.listen(5001, () => {
     console.log('running on port 5001');
 })
-mongoose.connect('mongodb://localhost/shopping_cart_db', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((res) => console.log('connect successfully'))
-    .catch((err) => {
-        console.log(err);
-    });
+
+db_config();
+// mongoose.connect('mongodb://localhost/shopping_cart_db', { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then((res) => console.log('connect successfully'))
+//     .catch((err) => {
+//         console.log(err);
+//     });
 
 
 
