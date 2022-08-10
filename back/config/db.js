7bb4889606f2c14c90bb434d74129d63fbf9ab85
AhmedAdel19 <adel.eng20@gmail.com>
2022-08-10 15:31:57 +0200

    finalProject

--- back/config/db.js
+++ back/config/db.js
@@ -1,10 +1,12 @@
+require('dotenv');
 const mongoose = require('mongoose');
 
 const connectionString = 'mongodb://localhost/shopping_cart_db';
 
+const ConnString = process.env.MONGO_URI || connectionString;
 
 function db_config() {
-    mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(ConnString, { useNewUrlParser: true, useUnifiedTopology: true })
         .then((res) => console.log('connect successfully'))
         .catch((err) => {
             console.log(err);
--- back/server.js
+++ back/server.js
@@ -1,18 +1,28 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const routers = require('./routes/routes');
 const db_config = require('./config/db')
+const PORT = process.env.PORT || 5001;
 
 app.use(bodyParser.json());
 
+console.log(process.env.NOODE_ENV)
 app.use('/', routers);
 
-app.listen(5001, () => {
+app.listen(PORT, () => {
     console.log('running on port 5001');
 })
 
 db_config();
+
+if (process.env.NOODE_ENV === "production") {
+    app.use("/", express.static("public"));
+    app.get("/", (req, res) => res.sendFile(__dirname + "/public/index.html"));
+} else {
+    app.get("/", (req, res) => res.send("API RUNNING..."));
+}
 
 
 
